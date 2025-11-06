c8e3e7165078517734e295b931837674780a3b8e
Fluvia20079668 <20079668@mydbs.ie>
2025-11-06 15:35:07 +0000

    Update Variables.tf

--- terraform/Variables.tf
+++ terraform/Variables.tf
@@ -1,19 +1,19 @@
 variable "aws_region" {
-  description = "AWS region to deploy resources in"
+  description = "AWS region"
   type        = string
   default     = "eu-north-1"
 }
 
-variable "instance_type" {
-  description = "EC2 instance type"
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
   type        = string
-  default     = "t3.micro"
+  default     = "ami-04b7b1c4371bda87b" # Amazon Linux 2 (EU-North-1)
 }
 
-variable "ami_id" {
-  description = "Amazon Linux 2023 AMI"
+variable "instance_type" {
+  description = "Instance type"
   type        = string
-  default     = "ami-07fb0a5bf9ae299a4"
+  default     = "t3.micro"
 }
 
 variable "ecr_repo_name" {
