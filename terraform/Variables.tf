47946c7d2ee551ed0c7ac5ec03dc0051376c97de
Fluvia20079668 <20079668@mydbs.ie>
2025-11-06 15:03:54 +0000

    Update Variables.tf

--- terraform/Variables.tf
+++ terraform/Variables.tf
@@ -1,16 +1,8 @@
-############################################
-# AWS Configuration
-############################################
-
 variable "aws_region" {
   description = "AWS region for deployment"
   type        = string
   default     = "eu-north-1"
 }
-
-############################################
-# EC2 Configuration
-############################################
 
 variable "ami_id" {
   description = "Amazon Linux 2023 AMI"
@@ -22,10 +14,4 @@ variable "ecr_name" {
   description = "Existing ECR repository name"
   type        = string
   default     = "my-simple-app"
-}
-
-variable "public_key_path" {
-  description = "Absolute path to your local SSH public key (OpenSSH format)"
-  type        = string
-  default     = "/Users/michaeljames/.ssh/id_rsa.pub"  # Change to your absolute path
 }
