9bf57fea8dffc4a435ace76ca94ff94ad9be3aa8
Fluvia20079668 <20079668@mydbs.ie>
2025-11-06 20:22:33 +0000

    Update Variables.tf

--- terraform/Variables.tf
+++ terraform/Variables.tf
@@ -1,17 +1,29 @@
-variable "aws_region" {
-  description = "AWS region to deploy resources in"
-  type        = string
-  default     = "eu-north-1"
-}
-
 variable "project_name" {
-  description = "Name prefix for resources"
+  description = "Prefix for all resource names"
   type        = string
   default     = "my-simple-app"
+}
+
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "eu-north-1"
 }
 
 variable "ecr_repo_name" {
   description = "Name of the ECR repository"
   type        = string
   default     = "my-simple-app"
+}
+
+variable "key_pair_name" {
+  description = "Name of the EC2 key pair"
+  type        = string
+  default     = "deployer-key"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
 }
